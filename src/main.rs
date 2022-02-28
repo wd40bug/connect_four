6ba610aebb0951fff58bddcdb7dc03efee697206
bobbobbob15 <wd40bug@gmail.com>
2022-02-28 14:35:10 -0700

    removed some unused imports

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,7 @@
-use simple_logger;
-use std::{fs, time::SystemTime, io};
-
-use connect_four::{solver::Solver, ai_stuff::Position, run};
+use connect_four::{run};
 
 
 fn main(){
-    simple_logger::init().unwrap();
-
-    // let mut seq = String::new();
-    // io::stdin().read_line(&mut seq).unwrap();
-    // let mut pos = Position::new();
-    // if !pos.set_up(seq.trim().to_string()){
-    //     println!("not a valid sequence");
-    // } else{
-
-    //     // println!("{}",pos.is_winning_move(6));
-
-    //     let now = SystemTime::now();
-    //     let score = solver.solve(&pos);
-    //     if let Ok(elapsed) = now.elapsed(){
-    //         println!("position: {}, score: {} nodes: {}, time in millis: {}",seq.trim(),score,solver.node_count,elapsed.as_micros());
-    //     }
-    // }
-
-    // 
-
-
     println!("{:?}",std::env::args().nth(1));
     let pretty = if let Some(arg) = std::env::args().nth(1){
         if arg.eq("debug"){
